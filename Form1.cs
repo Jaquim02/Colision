d5d3afa930cce296d8a1ab5dbb779f41a03ae078
Jaquim02 <122333533+Jaquim02@users.noreply.github.com>
2023-04-23 21:09:44 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace WindowsFormsAppColision
+{
+    public partial class Form1 : Form
+    {
+        int count;
+        private Bitmap bitmap;
+        static Graphics graphics;
+        ball[] balls;
+        public int boundY = 0, boundX = 0, boundX0, boundY0;
+        Boolean left;
+        Boolean right;
+        Boolean up;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            count = 0;
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+            pictureBox1.Image = bitmap;
+
+            // Crear las 25 pelotas
+            balls = new ball[25];
+            for (int i = 0; i < balls.Length; i++)
+            {
+                balls[i] = new ball();
+            }
+
+            // Suscribirse al evento Paint del PictureBox
+            pictureBox1.Paint += new PaintEventHandler(pictureBox1_Paint);
+        }
+
+
+
+        // Evento Paint del PictureBox
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Dibujar cada pelota
+            for (int i = 0; i < balls.Length; i++)
+            {
+                SolidBrush brush = new SolidBrush(balls[i].color);
+                e.Graphics.FillEllipse(brush, balls[i].x, balls[i].y, balls[i].diameter, balls[i].diameter);
+                balls[i].update(pictureBox1.Width - balls[i].diameter, pictureBox1.Height - balls[i].diameter, 0, 0);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Forzar el redibujado del PictureBox para llamar al evento Paint
+            pictureBox1.Invalidate();
+            count++;
+        }
+    }
+}
+
+
---
+++ ball.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppColision
+{
+    public class ball
+    {
+        Random rdan= new Random();
+        public int x;
+        public int y;
+        public int diameter;
+        public Point speed;
+        public Color color;
+
+
+        public ball()
+        {
+            this.x = rdan.Next(3,70);
+            this.y = rdan.Next(43, 80);
+            this.diameter = rdan.Next(5, 30);
+            this.color = Color.Blue;
+            speed.X = rdan.Next(4, 65);
+            speed.Y = rdan.Next(4, 7);
+        }
+
+        
+            public void update(int boundX, int boundY, int boundX0, int boundY0)
+            {
+                this.x += speed.X;
+                this.y += speed.Y;
+
+                if (this.x + diameter > boundX)
+                {
+                    this.speed.X = -Math.Abs(speed.X); // cambia la dirección horizontal de la velocidad de la bola
+                    this.x = boundX - diameter; // coloca la bola justo en la línea de límite
+                }
+                else if (this.x < boundX0)
+                {
+                    this.speed.X = Math.Abs(speed.X);
+                    this.x = boundX0;
+                }
+
+                if (this.y + diameter > boundY)
+                {
+                    this.speed.Y = -Math.Abs(speed.Y); // cambia la dirección vertical de la velocidad de la bola
+                    this.y = boundY - diameter;
+                }
+                else if (this.y < boundY0)
+                {
+                    this.speed.Y = Math.Abs(speed.Y);
+                    this.y = boundY0;
+                }
+            
+
+        }
+
+
+    }
+}
